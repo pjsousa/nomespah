207985699a293c6bde477507342b713c4d82518e
Pedro <pjgs.sousa@gmail.com>
2021-08-22 23:55:33 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nomespah",
-    version="0.0.1",
+    version="0.0.2",
     author="Pedro",
     author_email="pjgs.sousa@gmail.com",
     description="A name generator for tests and experiments",
